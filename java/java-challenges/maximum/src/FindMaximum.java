26e34918ee3d0c06c94d891098d08a561178dde9
Alberto Pintado <albertopintadolozano@gmail.com>
2021-05-03 10:56:25 +0200

    Updated paths

---
+++ java/java-challenges/maximum/src/FindMaximum.java
@@ -0,0 +1,16 @@
+class FindMaximum {
+  public static void main(String[] args) {
+    int[] numberList = {2, -6, 101, 100, 7};
+    System.out.println(maximum(numberList));
+  }
+
+  public static int maximum(int[] numberList) {
+    int maximumNumber = numberList[0];
+    for (int number : numberList) {
+      if (number > maximumNumber) {
+        maximumNumber = number;
+      }
+    }
+    return maximumNumber;
+  }
+}
